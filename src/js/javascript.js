780bdd6329fca57ffc40e5bf3b8b91674cc1650f
Lucioafk <luciogabriel1550@gmail.com>
2025-11-07 19:01:09 -0300

    Ajeitando a nav bar

---
+++ src/js/javascript.js
@@ -0,0 +1,10 @@
+const mobileBtn = document.querySelector('.btn-mobile');
+const navegar = document.querySelector('#nav');
+
+mobileBtn.addEventListener ('click', () => {
+    navegar.classList.toggle('show');
+    
+
+
+
+})
