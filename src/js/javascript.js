0339b9baf979589469b746c7ed2fbc38c68c9366
Lucioafk <luciogabriel1550@gmail.com>
2025-11-24 18:01:18 -0300

    atualizações

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,9 +1,11 @@
 const mobileBtn = document.querySelector('.btn-mobile');
 const navegar = document.querySelector('#nav');
+const icon = document.querySelector('.btn-mobile i');
 
 mobileBtn.addEventListener ('click', () => {
     navegar.classList.toggle('show');
-    
+    icon.classList.toggle('fa-times');
+    icon.classList.toggle('fa-bars');
 
 
 
